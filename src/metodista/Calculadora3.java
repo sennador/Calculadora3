cca56d353b0ac0e6b19bd54cea56dc52a1b57009
sennador <sennador@gmail.com>
2018-07-27 00:26:35 -0300

    meu primeiro commit na Calculadora3

---
+++ src/metodista/Calculadora3.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metodista;
+
+/**
+ *
+ * @author Ana Rafaela
+ */
+public class Calculadora3 {
+  public int somar (int a,int b) {
+      return a + b;
+  }
+  
+  public int subtrair (int a , int b){
+      return a - b;
+  }
+  
+  public int dividir (int a , int b){
+      return a / b;
+  }
+  public int multiplicar (int a , int b){
+      return a * b;
+  }
+  
+  public int modular (int a , int b){
+      return a % b;
+  }
+}
+
---
+++ test/metodista/Calculadora3Teste.java
@@ -0,0 +1,52 @@
+
+package metodista;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Anderson Rafael
+ */
+public class Calculadora3Teste {
+    
+    @Test
+    public void testarSoma(){
+        Calculadora3 calc3 = new Calculadora3();
+        assertEquals(calc3.somar(1,1), 2);
+        assertEquals(calc3.somar(1,0), 1);
+        assertEquals(calc3.somar(1,-1), 0);
+     }  
+   
+    @Test
+    public void testarSubtracao(){
+        Calculadora3 calc3 = new Calculadora3();
+        assertTrue(calc3.subtrair(1,1)==0);
+        assertTrue(calc3.subtrair(1,-1)==2);
+        assertTrue(calc3.subtrair(-1,1)==-2);
+    }
+    
+    @Test(expected=ArithmeticException.class)
+    public void testarDivisao(){
+        Calculadora3 calc3 = new Calculadora3();
+        assertEquals(calc3.dividir(3,2),1);
+        assertFalse (calc3.dividir(3, 3) == 3);
+        assertTrue (calc3.dividir(3,3)==1);
+        assertTrue (calc3.dividir(3, 0)==0);
+    }
+    @Test
+    public void testarMultiplicacao(){
+        Calculadora3 calc3 = new Calculadora3();
+       assertEquals(calc3.multiplicar(3,2),6);
+       assertTrue (calc3.multiplicar(3,3)==9);
+   
+    }
+    
+    @Test
+    public void testarModulo(){
+        Calculadora3 calc3 = new Calculadora3();
+        assertEquals(calc3.modular(8,3),2);
+    }
+    }
+    
+
