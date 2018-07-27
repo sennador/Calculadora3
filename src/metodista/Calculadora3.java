6ede5d7e4253ad33f3787a08ef01c2509d97c9eb
Ana Rafaela <Ana Rafaela@AnaRafaela-PC>
2018-07-27 00:50:42 -0300

    segundo commit na Calculadora3

--- src/metodista/Calculadora3.java
+++ src/metodista/Calculadora3.java
@@ -7,24 +7,26 @@ package metodista;
 
 /**
  *
- * @author Ana Rafaela
+ * @author Anderson Rafael (feito no notebook de Ana Rafaela)
  */
 public class Calculadora3 {
+    //metodo para somar dois numeros.
   public int somar (int a,int b) {
       return a + b;
   }
-  
+    //metodo para subtrair dois numeros.
   public int subtrair (int a , int b){
       return a - b;
   }
-  
+    //metodo para dividir dois numeros.
   public int dividir (int a , int b){
       return a / b;
   }
+    //metodo para multiplicar dois numeros.
   public int multiplicar (int a , int b){
       return a * b;
   }
-  
+    //metodo para saber o resto da divisao de dois numeros.
   public int modular (int a , int b){
       return a % b;
   }
